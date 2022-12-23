d1f7151a4c86046f5ed5aa76486061691453ef51
LeapEvgen <121282598+LeapEvgen@users.noreply.github.com>
2022-12-23 10:00:49 +0200

    Add files via upload

---
+++ ConsoleApplication3.cpp
@@ -0,0 +1,12 @@
+﻿#include <iostream.h>
+var n, k, s, i:word;
+begin
+read(n);
+k: = 0; s: = 0;
+for i: = 1 to n do
+if n mod i = 0 then
+begin
+write(i, ' '); k: = k + 1; s: = s + i;
+end;
+writeln(k, ' ', s);
+end; .
